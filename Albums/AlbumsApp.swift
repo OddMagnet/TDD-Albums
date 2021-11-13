3be5f25280b9e5fb048a06cc8da814be4d3f00ca
OddMagnet <mibruenen@gmail.com>
2021-11-13 08:12:32 +0100

    Initial Commit

---
+++ Albums/AlbumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumsApp.swift
+//  Albums
+//
+//  Created by Michael Brünen on 13.11.21.
+//
+
+import SwiftUI
+
+@main
+struct AlbumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
