09cf5fb16f3f54afadd98e041559fdb5d0723cc8
OddMagnet <mibruenen@gmail.com>
2021-12-26 21:50:35 +0100

    Add first test case

---
+++ AlbumsTests/NetworkImageSourceTests.m
@@ -0,0 +1,24 @@
+//
+//  NetworkImageSourceTests.m
+//  AlbumsTests
+//
+//  Created by Michael Brünen on 26.12.21.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NetworkImageSourceTestCase : XCTestCase
+
+@end
+
+@implementation NetworkImageSourceTestCase
+
+@end
+
+@implementation NetworkImageSourceTestCase (CreateImageSource)
+
+- (void)testCreateImageSource {
+    XCTAssert([NetworkImageSource createImageSource] == CGImageSourceCreateWithData);
+}
+
+@end
